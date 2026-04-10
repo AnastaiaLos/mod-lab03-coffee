0d47e2bd1e9f7840dc7bfd348ee0587c131dbf21
AnastaiaLos <anastasiy.los@yandex.ru>
2026-04-10 11:51:11 +0300

    Create Automata.h

---
+++ include/Automata.h
@@ -0,0 +1,34 @@
+#ifndef AUTOMATA_H
+#define AUTOMATA_H
+#include <string>
+#include <vector>
+enum class STATES {
+    OFF,
+    WAIT,
+    ACCEPT,
+    CHECK,
+    COOK
+};
+class Automata {
+private:
+    int cash;
+    int selectedPrice;
+    std::vector<std::string> menu;
+    std::vector<int> prices;
+    STATES state;
+public:
+    Automata();
+    void on();
+    void off();
+    void coin(int amount);
+    std::string getMenu();
+    STATES getState();
+    bool choice(int drinkIndex);
+    void cancel();
+    void cook();
+    void finish();
+    int getCash() const;
+    int getSelectedPrice() const;
+    void setStateForTest(STATES newState);
+};
+#endif
