204fe3fb3d8b241864a7be1856d7f73edcc53186
AnastaiaLos <anastasiy.los@yandex.ru>
2026-04-10 11:52:05 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "Automata.h"
+#include <iostream>
+int main() {
+    Automata coffeeMachine;
+    std::cout << coffeeMachine.getMenu();
+    coffeeMachine.on();
+    coffeeMachine.coin(100);
+    coffeeMachine.choice(2);
+    return 0;
+}
